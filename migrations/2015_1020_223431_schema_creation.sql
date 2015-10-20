5ca315b000b825c1d17b04843fd6329fe2e6882d
Arthur Verkaik <aquarthur@yahoo.com>
2015-10-20 22:45:34 +0100

    SQL Script which creates the database schema.
    Decided to move to a 'migrations' system as opposed to having one
    big script, as it will be easier to find where things went wrong
    and provides a more organized view of the database.

---
+++ migrations/2015_1020_223431_schema_creation.sql
@@ -0,0 +1,24 @@
+# Users table
+CREATE TABLE Users (
+  userID uuid PRIMARY KEY,
+  username varchar(12) NOT NULL,
+  email varchar(50) NOT NULL,
+  firstName varchar(30) NOT NULL,
+  lastName varchar(30) NOT NULL,
+  password varchar(100) NOT NULL
+);
+
+# Table for the raspberry pi's, links them to the users they're connected to.
+CREATE TABLE Raspberries (
+  raspberryID uuid PRIMARY KEY,
+  userID uuid REFERENCES users(userID)
+);
+
+# Events table, linked to the pi (which is linked to different users)
+CREATE TABLE Events (
+  raspberryID uuid REFERENCES raspberries(raspberryID),
+  eventID varchar(10) NOT NULL,
+  eventTime timestamp DEFAULT current_timestamp,
+  description text,
+  PRIMARY KEY(raspberryID,eventID,eventTime)
+);
