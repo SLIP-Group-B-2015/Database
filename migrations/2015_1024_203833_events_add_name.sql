b46b5798b47ac6294cecd6e1f36cd58b6522f922
Arthur Verkaik <aquarthur@yahoo.com>
2015-10-24 20:45:25 +0100

    Added name column to Events and renamed description -> note.

---
+++ migrations/2015_1024_203833_events_add_name.sql
@@ -0,0 +1,6 @@
+-- Written by Arthur Verkaik
+-- Adds a new field in Events called name (firstName + lastName from Users)
+-- Also changes the name of the description field to note.
+
+ALTER TABLE Events ADD COLUMN name varchar(50);
+ALTER TABLE Events RENAME COLUMN description TO note;
