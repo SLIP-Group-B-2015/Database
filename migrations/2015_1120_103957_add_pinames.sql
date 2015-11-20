7409cb0fb79dc3f8e86e0f9f3453c76db4b01b8d
Arthur Verkaik <aquarthur@yahoo.com>
2015-11-20 13:21:38 +0000

    Added a name field in the Raspberries table.
    This will allow users to name their raspberry pi's for easier
    reference in the future (rather than having to type in the full
    UUID). Also, changed the primary in the table to (raspberryID, userID)
    in order to allow multiple users per raspberry. To deal with this
    change (as raspberryID no longer uniquely identifies every entry
    in the table), Events' foreign key for raspberryid was removed.

---
+++ migrations/2015_1120_103957_add_pinames.sql
@@ -0,0 +1,10 @@
+-- Written by Arthur Verkaik
+
+-- Add a name field in Raspberries which will be used to let users name their
+-- raspberries.
+-- Also, change table Raspberries so it can now let multiple users connect to
+-- a single raspberry by changing the primary key to (raspberryID, userID).
+
+ALTER TABLE Raspberries DROP CONSTRAINT raspberries_pkey CASCADE;
+ALTER TABLE Raspberries ADD PRIMARY KEY (raspberryID, userID);
+ALTER TABLE Raspberries ADD COLUMN name varchar(30);
